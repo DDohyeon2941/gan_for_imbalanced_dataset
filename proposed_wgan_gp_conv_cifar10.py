7379100f46e8c46244e5da3e1cb973100a8dd6c2
DDohyeon2941 <51852452+DDohyeon2941@users.noreply.github.com>
2023-11-10 15:02:42 +0900

    제안알고리즘 간단구현
    wgan_gp를 대상으로 cifar-10을 사용해서, 제안알고리즘을 간단하게 구현해봄. 실험도 진행. tensorbaord를 사용한 버전도 있기는 한데, 서버에서 바로 보기는 어려움

--- proposed_wgan_gp_conv_cifar10.py
+++ proposed_wgan_gp_conv_cifar10.py
@@ -14,6 +14,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import torch.optim as optim
 from torch.autograd import grad
+from torchvision.utils import make_grid
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -21,12 +22,12 @@ transform = transforms.Compose([
 ])
 
 
-batch_size = 128
+batch_size = 64
 train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=False)
 
 #train_loader = DataLoader(train_dataset, batch_size= batch_size, shuffle=True, drop_last=True)
 
-minority_data = [(data, label) for data, label in train_dataset if label in [3,]]
+minority_data = [(data, label) for data, label in train_dataset if label in [5,]]
 minority_loader = DataLoader(minority_data, batch_size= batch_size, shuffle=True, drop_last=True)
 
 
@@ -98,6 +99,27 @@ def cal_gradient(t_gradients, t_lambda1):
     gradient_penalty = t_lambda1 * (deviations ** 2).mean()
 
     return gradient_penalty
+
+# 훈련 중간에 이미지를 시각화하는 함수
+def show_generated_imgs(generator, z_dim, device, num_images=64):
+    # 잠재 벡터 z 생성
+    z = torch.randn(num_images, z_dim, 1, 1, device=device)
+    
+    # 이미지 생성
+    generator.eval() # 생성자를 평가 모드로 설정
+    with torch.no_grad(): # 그라디언트 계산을 하지 않음
+        fake_images = generator(z).detach().cpu()
+    generator.train() # 생성자를 훈련 모드로 되돌림
+
+    # 이미지의 픽셀 값을 [0, 1] 범위로 정규화
+    fake_images = make_grid(fake_images, normalize=True, nrow=int(np.sqrt(num_images)))
+    
+    # 이미지 시각화
+    plt.figure(figsize=(8, 8))
+    plt.imshow(np.transpose(fake_images.numpy(), (1, 2, 0)))
+    plt.axis('off')
+    plt.show()
+
 #%%
 
 # 하이퍼파라미터
@@ -105,8 +127,9 @@ z_dim = 100
 img_dim = 32*32
 lr = 0.0001
 #batch_size = 128
-num_epochs = 200
-lambda1 = 10
+num_epochs = 100
+lambda1 = 20
+lambda2 = 0.5
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -116,13 +139,19 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # 모델 및 옵티마이저 초기화
 generator = nn.DataParallel(Generator(z_dim))
 generator.to(device)
-discriminator = nn.DataParallel(Discriminator())
-discriminator.to(device)
+discriminator1 = nn.DataParallel(Discriminator())
+discriminator1.to(device)
 
+
+
+discriminator2 = nn.DataParallel(Discriminator())
+discriminator2.load_state_dict(torch.load('discriminator.pth'))
+discriminator2.to(device)
 
 
 g_optimizer = optim.RMSprop(generator.parameters(), lr=lr)
-d_optimizer = optim.RMSprop(discriminator.parameters(), lr=lr)
+d_optimizer1 = optim.RMSprop(discriminator1.parameters(), lr=lr)
+#d_optimizer2 = optim.RMSprop(discriminator2.parameters(), lr=lr)
 
 
 ###
@@ -134,13 +163,16 @@ for epoch in range(num_epochs):
         real_data = real_data.to(device)
         # 판별자 학습
         for _ in range(5):
-            d_optimizer.zero_grad()
+            d_optimizer1.zero_grad()
+            #d_optimizer2.zero_grad()
+
 
             z = torch.randn(batch_size, z_dim,1,1).to(device)
             fake_images = generator(z)
 
-            d_loss_real = discriminator(real_data)
-            d_loss_fake = discriminator(fake_images)
+            d_loss_real = discriminator1(real_data)
+            d_loss_fake_min = discriminator1(fake_images)
+            d_loss_fake_maj = discriminator2(fake_images)
 
             alpha = torch.rand(batch_size, 1,1,1).to(device)
             alpha.to(device)
@@ -148,16 +180,27 @@ for epoch in range(num_epochs):
             x_hat = (alpha * real_data + (1 - alpha) * fake_images).detach()
             x_hat.requires_grad = True
 
-            pred_hat = discriminator(x_hat)
-            gradients = grad(outputs=pred_hat, inputs=x_hat, grad_outputs=torch.ones(pred_hat.size()).to(device),
+            pred_hat_min = discriminator1(x_hat)
+            #pred_hat_maj = discriminator2(x_hat)
+
+            gradients_min = grad(outputs=pred_hat_min, inputs=x_hat, grad_outputs=torch.ones(pred_hat_min.size()).to(device),
                                              create_graph=True, retain_graph=True, only_inputs=True)[0]
 
-            gradient_penalty = cal_gradient(gradients, lambda1)
+            """
+            gradients_maj = grad(outputs=pred_hat_maj, inputs=x_hat, grad_outputs=torch.ones(pred_hat_maj.size()).to(device),
+                                             create_graph=True, retain_graph=True, only_inputs=True)[0]
+            """
+            gradient_penalty_min = cal_gradient(gradients_min, lambda1)
+            #gradient_penalty_maj = cal_gradient(gradients_maj, lambda1)
 
-            d_loss = -torch.mean(d_loss_real) + torch.mean(d_loss_fake) + gradient_penalty
+            #output_diff = torch.abs(d_loss_fake_min  - d_loss_fake_maj)
+
+            d_loss = -torch.mean(d_loss_real) + (1*torch.mean(d_loss_fake_min)) + gradient_penalty_min + (0*torch.mean(d_loss_fake_maj))
+
+            #d_loss = 0.6 * (-torch.mean(d_loss_real) + torch.mean(d_loss_fake_min) + gradient_penalty_min) + 0.4 * torch.mean(d_loss_fake_maj)
 
             d_loss.backward()
-            d_optimizer.step()
+            d_optimizer1.step()
 
 
         #생성자 학습
@@ -165,9 +208,10 @@ for epoch in range(num_epochs):
 
         z = torch.randn(batch_size, z_dim, 1, 1).to(device)
         fake_images = generator(z)
-        output = discriminator(fake_images)
+        output_min = discriminator1(fake_images)
+        output_maj = discriminator2(fake_images)
 
-        g_loss = -torch.mean(output)
+        g_loss = -(1*torch.mean(output_min)) - (0*torch.mean(output_maj))
 
         g_loss.backward()
         g_optimizer.step()
@@ -175,8 +219,9 @@ for epoch in range(num_epochs):
 
         loss_dict['d_loss'].append(d_loss.item())
         loss_dict['g_loss'].append(g_loss.item())
-
         print(f"Epoch [{epoch + 1}/{num_epochs}] D Loss: {d_loss.item()} G Loss: {g_loss.item()}")
+    if epoch % 10 == 0:
+        show_generated_imgs(generator, z_dim, device)
 
 
 #%%
@@ -227,8 +272,6 @@ plt.legend()
 
 #%%
 
-torch.save(generator.state_dict(), 'generator.pth')
-torch.save(discriminator.state_dict(), 'discriminator.pth')
 
 
 
--- proposed_wgan_gp_conv_cifar10_with_tensorboard.py
+++ proposed_wgan_gp_conv_cifar10_with_tensorboard.py
@@ -14,19 +14,21 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import torch.optim as optim
 from torch.autograd import grad
-
+from torchvision.utils import make_grid
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 
-batch_size = 128
+batch_size = 64
 train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=False)
 
 #train_loader = DataLoader(train_dataset, batch_size= batch_size, shuffle=True, drop_last=True)
 
-minority_data = [(data, label) for data, label in train_dataset if label in [3,]]
+minority_data = [(data, label) for data, label in train_dataset if label in [5,]][:500]
 minority_loader = DataLoader(minority_data, batch_size= batch_size, shuffle=True, drop_last=True)
 
 
@@ -98,15 +100,40 @@ def cal_gradient(t_gradients, t_lambda1):
     gradient_penalty = t_lambda1 * (deviations ** 2).mean()
 
     return gradient_penalty
+
+# 훈련 중간에 이미지를 시각화하는 함수
+def show_generated_imgs(generator, z_dim, device, num_images=64):
+    # 잠재 벡터 z 생성
+    z = torch.randn(num_images, z_dim, 1, 1, device=device)
+    
+    # 이미지 생성
+    generator.eval() # 생성자를 평가 모드로 설정
+    with torch.no_grad(): # 그라디언트 계산을 하지 않음
+        fake_images = generator(z).detach().cpu()
+    generator.train() # 생성자를 훈련 모드로 되돌림
+
+    # 이미지의 픽셀 값을 [0, 1] 범위로 정규화
+    fake_images = make_grid(fake_images, normalize=True, nrow=int(np.sqrt(num_images)))
+    
+    # 이미지 시각화
+    plt.figure(figsize=(8, 8))
+    plt.imshow(np.transpose(fake_images.numpy(), (1, 2, 0)))
+    plt.axis('off')
+    plt.show()
+
 #%%
+
+# SummaryWriter 인스턴스를 생성합니다.
+writer = SummaryWriter('gan_runs/GAN_experiment')
 
 # 하이퍼파라미터
 z_dim = 100
 img_dim = 32*32
 lr = 0.0001
 #batch_size = 128
-num_epochs = 200
-lambda1 = 10
+num_epochs = 300
+lambda1 = 20
+lambda2 = 0.5
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -116,31 +143,40 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # 모델 및 옵티마이저 초기화
 generator = nn.DataParallel(Generator(z_dim))
 generator.to(device)
-discriminator = nn.DataParallel(Discriminator())
-discriminator.to(device)
+discriminator1 = nn.DataParallel(Discriminator())
+discriminator1.to(device)
 
+
+
+discriminator2 = nn.DataParallel(Discriminator())
+discriminator2.load_state_dict(torch.load('discriminator.pth'))
+discriminator2.to(device)
 
 
 g_optimizer = optim.RMSprop(generator.parameters(), lr=lr)
-d_optimizer = optim.RMSprop(discriminator.parameters(), lr=lr)
+d_optimizer1 = optim.RMSprop(discriminator1.parameters(), lr=lr)
+#d_optimizer2 = optim.RMSprop(discriminator2.parameters(), lr=lr)
 
 
 ###
 loss_dict = {'d_loss':[], 'g_loss':[]}
 
 for epoch in range(num_epochs):
-    for real_data, _ in minority_loader:
+    for i, (real_data, _) in enumerate(minority_loader):
         #real_data = real_data.view(real_data.size(0),-1).to(device)
         real_data = real_data.to(device)
         # 판별자 학습
         for _ in range(5):
-            d_optimizer.zero_grad()
+            d_optimizer1.zero_grad()
+            #d_optimizer2.zero_grad()
+
 
             z = torch.randn(batch_size, z_dim,1,1).to(device)
             fake_images = generator(z)
 
-            d_loss_real = discriminator(real_data)
-            d_loss_fake = discriminator(fake_images)
+            d_loss_real = discriminator1(real_data)
+            d_loss_fake_min = discriminator1(fake_images)
+            d_loss_fake_maj = discriminator2(fake_images)
 
             alpha = torch.rand(batch_size, 1,1,1).to(device)
             alpha.to(device)
@@ -148,16 +184,27 @@ for epoch in range(num_epochs):
             x_hat = (alpha * real_data + (1 - alpha) * fake_images).detach()
             x_hat.requires_grad = True
 
-            pred_hat = discriminator(x_hat)
-            gradients = grad(outputs=pred_hat, inputs=x_hat, grad_outputs=torch.ones(pred_hat.size()).to(device),
+            pred_hat_min = discriminator1(x_hat)
+            pred_hat_maj = discriminator2(x_hat)
+
+            gradients_min = grad(outputs=pred_hat_min, inputs=x_hat, grad_outputs=torch.ones(pred_hat_min.size()).to(device),
                                              create_graph=True, retain_graph=True, only_inputs=True)[0]
 
-            gradient_penalty = cal_gradient(gradients, lambda1)
+            """
+            gradients_maj = grad(outputs=pred_hat_maj, inputs=x_hat, grad_outputs=torch.ones(pred_hat_maj.size()).to(device),
+                                             create_graph=True, retain_graph=True, only_inputs=True)[0]
+            """
+            gradient_penalty_min = cal_gradient(gradients_min, lambda1)
+            #gradient_penalty_maj = cal_gradient(gradients_maj, lambda1)
 
-            d_loss = -torch.mean(d_loss_real) + torch.mean(d_loss_fake) + gradient_penalty
+            output_diff = torch.abs(d_loss_fake_min  - d_loss_fake_maj)
+
+            d_loss = -torch.mean(d_loss_real) + (0.9*torch.mean(d_loss_fake_min)) + gradient_penalty_min + (0.1*torch.mean(d_loss_fake_maj)) + (0.2 * output_diff.mean())
+
+            #d_loss = 0.6 * (-torch.mean(d_loss_real) + torch.mean(d_loss_fake_min) + gradient_penalty_min) + 0.4 * torch.mean(d_loss_fake_maj)
 
             d_loss.backward()
-            d_optimizer.step()
+            d_optimizer1.step()
 
 
         #생성자 학습
@@ -165,9 +212,10 @@ for epoch in range(num_epochs):
 
         z = torch.randn(batch_size, z_dim, 1, 1).to(device)
         fake_images = generator(z)
-        output = discriminator(fake_images)
+        output_min = discriminator1(fake_images)
+        output_maj = discriminator2(fake_images)
 
-        g_loss = -torch.mean(output)
+        g_loss = -(0.9*torch.mean(output_min)) - (0.1*torch.mean(output_maj))
 
         g_loss.backward()
         g_optimizer.step()
@@ -175,10 +223,25 @@ for epoch in range(num_epochs):
 
         loss_dict['d_loss'].append(d_loss.item())
         loss_dict['g_loss'].append(g_loss.item())
-
         print(f"Epoch [{epoch + 1}/{num_epochs}] D Loss: {d_loss.item()} G Loss: {g_loss.item()}")
+   # Epoch마다 생성된 이미지들을 TensorBoard에 기록합니다.
+    with torch.no_grad():
+        # 고정된 잠재 벡터를 사용하여 일관성 있는 이미지를 생성합니다.
+        # 예를 들어, 스칼라 값 로깅
+        writer.add_scalar('generator loss', g_loss.item(), epoch * len(minority_loader) + i)
+        writer.add_scalar('discriminator loss', d_loss.item(), epoch * len(minority_loader) + i)
+
+        fixed_z = torch.randn(64, z_dim, 1, 1).to(device)
+        fake_images = generator(fixed_z).detach().cpu()
+        img_grid = torchvision.utils.make_grid(fake_images, normalize=True, scale_each=True)
+        writer.add_image('Generated Images', img_grid, global_step=epoch)
+    if epoch % 10 == 0:
+        show_generated_imgs(generator, z_dim, device)
 
 
+
+# 학습이 끝나면 writer를 닫습니다.
+writer.close()
 #%%
 
 # 학습된 생성자를 이용해서 가짜 샘플 생성
@@ -227,8 +290,7 @@ plt.legend()
 
 #%%
 
-torch.save(generator.state_dict(), 'generator.pth')
-torch.save(discriminator.state_dict(), 'discriminator.pth')
+torch.save(generator.state_dict(), 'generator_prop.pth')
 
 
 
